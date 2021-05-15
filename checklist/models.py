4a1ddf657509061b9c33f7f5b6ab3ebf4f127664
Luiz Gustavo A Lima <luizlima@Luizs-MacBook-Pro.local>
2021-05-15 20:59:27 -0300

    Criado as tabelas Evento e Check. Criado a view lista check

--- checklist/models.py
+++ checklist/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+ESTADO_CHOICES = (
+    ('ativo', 'Ativo'),
+    ('inativo', 'Inativo'),
+)
+
+class Evento(models.Model):
+
+    nome = models.CharField(max_length=255, null=False)
+    estado = models.CharField(max_length=7,
+                              choices=ESTADO_CHOICES,
+                              default='ativo',
+                              )
+    def __str__(self):
+        return self.nome
+
+class Check(models.Model):
+    evento = models.ForeignKey(Evento,
+                               on_delete=models.CASCADE,
+                               related_name='Check_Evento',
+                               null=True,
+                               blank=True)
+    datacriacao = models.DateTimeField(auto_now_add=True)
+    dataatualizacao = models.DateTimeField(auto_now=True)
+    observacao = models.CharField(max_length=255, null=True)
+    estado = models.CharField(max_length=7,
+                              choices=ESTADO_CHOICES,
+                              default='ativo',
+                              )
+    object = models.Manager()
+
+    def __int__(self):
+        return self.evento
--- checklist/views.py
+++ checklist/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Check
 
 def CheckList(request):
-    pass
+    checks = Check.object.filter(estado='ativo')
+    return render(request, 'checklist/lista.html', {'checks': checks})
